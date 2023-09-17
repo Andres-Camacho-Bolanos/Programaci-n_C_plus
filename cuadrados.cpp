67dbeb307b3c4006003f4fe43b186e0d310eb48e
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2023-09-16 18:49:41 -0600

    Update cuadrados.cpp
    Se añadieron líneas de código que imprimen los valores de la diagonal y posición de cada punto en el plano cartesiano. Estas se calculan a partir de funciones que ya se habían declarado previamente.

--- cuadrados.cpp
+++ cuadrados.cpp
@@ -23,7 +23,7 @@ using namespace std;
 
 // Definición de la estructura para representar un punto en el plano cartesiano.
 struct Punto {
-    double x, y; //Se declara nuestro tipo de dato como doble flotante.
+    double x, y; // Se declaran nuestros tipos de dato como doble flotante.
 };
 
 // Función para calcular la distancia entre dos puntos.
@@ -49,10 +49,16 @@ bool esCuadrado(Punto puntos[4]) {
     sort(distancias, distancias + 6);
 
     // Comprueba si las distancias forman un cuadrado.
-    return distancias[0] == distancias[1] && 
-           distancias[1] == distancias[2] && 
-           distancias[2] == distancias[3] &&
-           distancias[4] == distancias[5];
+    if (distancias[0] == distancias[1] && 
+        distancias[1] == distancias[2] && 
+        distancias[2] == distancias[3] &&
+        distancias[4] == distancias[5]) {
+        cout << "Las distancias son iguales: " << distancias[0] << endl;
+        cout << "Longitud de la diagonal: " << distancias[4] << endl;
+        return true;
+    }
+
+    return false;
 }
 
 // Función para determinar si un conjunto de 4 puntos forma un rectángulo.
@@ -73,8 +79,14 @@ bool esRectangulo(Punto puntos[4]) {
     sort(distancias, distancias + 6);
 
     // Comprueba si las distancias forman un rectángulo.
-    return distancias[0] == distancias[1] && 
-           distancias[2] == distancias[3];
+    if (distancias[0] == distancias[1] && 
+        distancias[2] == distancias[3]) {
+        cout << "Las distancias son iguales: " << distancias[0] << endl;
+        cout << "Longitud de la diagonal: " << distancias[4] << endl;
+        return true;
+    }
+
+    return false;
 }
 
 int main() {
@@ -88,6 +100,12 @@ int main() {
         cin >> puntos[i].y;
     }
 
+    // Imprime las ubicaciones de los puntos.
+    cout << "Ubicación de los puntos:" << endl;
+    for (int i = 0; i < 4; i++) {
+        cout << "Punto " << i + 1 << ": (" << puntos[i].x << ", " << puntos[i].y << ")" << endl;
+    }
+
     // Verifica si los puntos forman un cuadrado o un rectángulo.
     if (esCuadrado(puntos)) {
         cout << "Los puntos forman un cuadrado." << endl;
@@ -95,7 +113,7 @@ int main() {
         cout << "Los puntos forman un rectángulo." << endl;
     } else {
         cout << "Los puntos no forman un cuadrado ni un rectángulo." << endl;
-        cout << "La figura es un paralelogramo." << endl;
+        cout << "La figura es un cuadrángulo." << endl;
     }
 
     return 0;
